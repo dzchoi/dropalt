e19809233b08de9e403f3900f48e5b16fd755b8f
dzchoi <19231860+dzchoi@users.noreply.github.com>
2022-10-20 18:59:46 -0400

    keymap: Fix _any_t::enroll/unenroll() to be able to perform enroll/unenroll() from on_other_press/release().

--- keymap/observer.cpp
+++ keymap/observer.cpp
@@ -6,12 +6,15 @@ namespace key {
 
 bool _any_t::enroll(observer_t* observer)
 {
-    if ( observer->next == nullptr && m_observers != observer ) {
-        observer->next = m_observers;
-        m_observers = observer;
-        return true;
-    }
-    return false;
+    observer_t** pnext = &m_observers;
+    for ( ; *pnext ; pnext = &(*pnext)->next )
+        if ( *pnext == observer )
+            return false;
+    // Append it to the end of the list. Its own on_other_press/release() will be also
+    // called when enroll() is performed from on_other_press/release().
+    observer->next = nullptr;
+    *pnext = observer;
+    return true;
 }
 
 bool _any_t::unenroll(observer_t* observer)
@@ -19,7 +22,8 @@ bool _any_t::unenroll(observer_t* observer)
     for ( observer_t** pnext = &m_observers ; *pnext ; pnext = &(*pnext)->next )
         if ( *pnext == observer ) {
             *pnext = observer->next;
-            observer->next = nullptr;
+            // We do not clear here observer->next to be able to perform unenroll() from
+            // on_other_press/release().
             return true;
         }
     return false;
