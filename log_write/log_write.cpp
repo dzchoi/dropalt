47398fd295bf16cb87b4aa7952785305cce20547
dzchoi <19231860+dzchoi@users.noreply.github.com>
2025-02-24 20:11:44 -0500

    `log_write_safe()` is thread-safe and preserves color integrity when called concurrently.

--- log_write/log_write.cpp
+++ log_write/log_write.cpp
@@ -1,49 +1,34 @@
-#include "log.h"                // for LOG_ERROR, LOG_WARNING and LOG_INFO
+#include "log.h"                // for LOG_ERROR, LOG_WARNING, ...
 #include "log_module.h"
 #include "mutex.h"
 
 #include <cstdarg>              // for va_start(), va_end()
-#include <cstdio>               // for stdout, fputs(), vprintf()
-// #include "persistent.hpp"       // for persistent::add_log()
+#include <cstdio>               // for fputs(), vprintf()
 
 
+
+static constexpr const char* COLOR_CODE[] = {
+    "\e[0m",     // LOG_NONE (not used)
+    "\e[0;31m",  // LOG_ERROR (red)
+    "\e[0;33m",  // LOG_WARNING (yellow)
+    "\e[0;36m",  // LOG_INFO (cyan)
+    "\e[0m"      // LOG_DEBUG
+};
 
 void log_write_safe(unsigned level, const char* format, ...)
 {
-    static mutex_t log_mutex = MUTEX_INIT;
-    mutex_lock(&log_mutex);
+    va_list args;
+    va_start(args, format);
 
-    va_list args1;
-    va_start(args1, format);
-
-    const char* font_color = nullptr;
-    switch ( level ) {
-        case LOG_ERROR:
-            // va_list args2;
-            // va_copy(args2, args1);
-            // persistent::add_log(format, args2);
-            // va_end(args2);
-
-            font_color = "\e[0;31m";  // red
-            break;
-
-        case LOG_WARNING:
-            font_color = "\e[0;33m";  // yellow
-            break;
-
-        case LOG_INFO:
-            font_color = "\e[0;36m";  // cyan
-            break;
+    static mutex_t coloring_lock = MUTEX_INIT;
+    mutex_lock(&coloring_lock);  // Safe to call even in an interrupt context.
+    static unsigned last_level = LOG_DEBUG;
+    if ( level != last_level ) {
+        fputs(COLOR_CODE[level], stdout);
+        last_level = level;
     }
+    mutex_unlock(&coloring_lock);
 
-    if ( font_color )
-        fputs(font_color, stdout);
-
-    vprintf(format, args1);
-    va_end(args1);
-
-    if ( font_color )
-        fputs("\e[0m", stdout);
-
-    mutex_unlock(&log_mutex);
+    vprintf(format, args);
+    va_end(args);
 }
